e5aee81745c5d5510bcf35925dd2cc3ecab6f85a
HeewonKwak <hewo1217@gmail.com>
2023-09-05 11:28:28 +0900

    feat: add DFS

---
+++ programmers/DFS_BFS/네트워크.py
@@ -0,0 +1,14 @@
+def dfs(now, visited, nodes):
+    visited[now] = True
+    for i in range(len(nodes[now])):
+        if visited[i] == False and nodes[now][i] == 1:
+            dfs(i, visited, nodes)
+            
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    for i in range(n):
+        if visited[i] == False:
+            dfs(i, visited, computers)
+            answer += 1
+    return answer
