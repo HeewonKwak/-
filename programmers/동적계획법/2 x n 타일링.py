685808a560463dfbcd92ecdf6306d8795d954b47
HeewonKwak <hewo1217@gmail.com>
2023-12-12 15:42:09 +0900

    feat: add programmers - 기출 1 - stack queue 1 - DP 1

---
+++ programmers/동적계획법/2 x n 타일링.py
@@ -0,0 +1,10 @@
+# 2 x n 타일링
+# https://school.programmers.co.kr/learn/courses/30/lessons/12900
+def solution(n):
+    answer = 0
+    dp = [0] * (n + 2)
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(1, n):
+        dp[i+2] = dp[i+1] % 1000000007 + dp[i]
+    return dp[n+1] % 1000000007
